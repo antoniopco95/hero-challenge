c4836b766bc3760ccfbb572e8668cbf453e92407
antoniopco95 <antoniopco95@gmail.com>
2023-10-29 11:25:57 -0300

    Montando estrutura do contexto

---
+++ src/hooks/useUserProvider.jsx
@@ -0,0 +1,5 @@
+function useUserProvider() {
+  return {};
+}
+
+export default useUserProvider;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,17 @@
 import "./styles.css";
 import Sidebar from "../../components/Sidebar";
+import HeroCard from "../../components/HeroCard";
 
 function Home() {
   return (
     <>
       <div className="container">
         <Sidebar />
-        <div className="main-content"></div>
+        <div className="main-content">
+          <div className="cards-area">
+            <HeroCard />
+          </div>
+        </div>
       </div>
     </>
   );
